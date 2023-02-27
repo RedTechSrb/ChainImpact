974f34c68d903b432846f1f78dc07eae73c6c0b4
DjordjeD <serbiasistem@gmail.com>
2023-02-27 14:56:20 +0100

    add testimonials, cards

---
+++ chain_impact_web/src/components/ProjectExplorer.tsx
@@ -0,0 +1,161 @@
+import {
+  createStyles,
+  SimpleGrid,
+  Card,
+  Image,
+  Text,
+  Container,
+  AspectRatio,
+  TextInput,
+  ActionIcon,
+  useMantineTheme,
+  Title,
+} from "@mantine/core";
+import { IconArrowLeft, IconArrowRight, IconSearch } from "@tabler/icons";
+
+const mockdata = [
+  {
+    title: "Top 10 places to visit in Norway this summer",
+    image:
+      "https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+    date: "August 18, 2022",
+  },
+  {
+    title: "Best forests to visit in North America",
+    image:
+      "https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+    date: "August 27, 2022",
+  },
+  {
+    title: "Hawaii beaches review: better than you think",
+    image:
+      "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+    date: "September 9, 2022",
+  },
+  {
+    title: "Mountains at night: 12 best locations to enjoy the view",
+    image:
+      "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+    date: "September 12, 2022",
+  },
+  {
+    title: "Hawaii beaches review: better than you think",
+    image:
+      "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+    date: "September 9, 2022",
+  },
+  {
+    title: "Mountains at night: 12 best locations to enjoy the view",
+    image:
+      "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
+    date: "September 12, 2022",
+  },
+];
+
+const useStyles = createStyles((theme) => ({
+  card: {
+    transition: "transform 150ms ease, box-shadow 150ms ease",
+
+    "&:hover": {
+      transform: "scale(1.01)",
+      boxShadow: theme.shadows.md,
+    },
+  },
+
+  herotitle: {
+    fontSize: 34,
+    fontWeight: 900,
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: 24,
+    },
+  },
+
+  description: {
+    maxWidth: 600,
+    margin: "auto",
+
+    "&::after": {
+      content: '""',
+      display: "block",
+      backgroundColor: theme.fn.primaryColor(),
+      width: 45,
+      height: 2,
+      marginTop: theme.spacing.sm,
+      marginLeft: "auto",
+      marginRight: "auto",
+    },
+  },
+
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    fontWeight: 600,
+  },
+}));
+
+export default function ProjectExplorer() {
+  const { classes } = useStyles();
+  const theme = useMantineTheme();
+
+  const cards = mockdata.map((article) => (
+    <Card
+      key={article.title}
+      p="md"
+      radius="md"
+      component="a"
+      href="#"
+      className={classes.card}
+    >
+      <AspectRatio ratio={1920 / 1080}>
+        <Image src={article.image} />
+      </AspectRatio>
+      <Text color="dimmed" size="xs" transform="uppercase" weight={700} mt="md">
+        {article.date}
+      </Text>
+      <Text className={classes.title} mt={5}>
+        {article.title}
+      </Text>
+    </Card>
+  ));
+
+  return (
+    <Container py="xl" size="lg">
+      <Title order={2} className={classes.herotitle} align="center" mt="sm">
+        Find the Biggest Impactors
+      </Title>
+
+      <Text
+        color="dimmed"
+        className={classes.description}
+        align="center"
+        mt="md"
+      >
+        See which communities have the strongest social presence and which
+        community members make it all happen!
+      </Text>
+      <TextInput
+        icon={<IconSearch size={18} stroke={1.5} />}
+        radius="xl"
+        size="md"
+        rightSection={
+          <ActionIcon
+            size={32}
+            radius="xl"
+            color={theme.primaryColor}
+            variant="filled"
+          >
+            {theme.dir === "ltr" ? (
+              <IconArrowRight size={18} stroke={1.5} />
+            ) : (
+              <IconArrowLeft size={18} stroke={1.5} />
+            )}
+          </ActionIcon>
+        }
+        placeholder="Search questions"
+        rightSectionWidth={42}
+      />
+      <SimpleGrid cols={3} breakpoints={[{ maxWidth: "sm", cols: 1 }]}>
+        {cards}
+      </SimpleGrid>
+    </Container>
+  );
+}

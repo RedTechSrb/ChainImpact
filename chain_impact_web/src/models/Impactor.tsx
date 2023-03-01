5e537d5ca1a2078d76c7a89ee287ef79a36519dc
Miloš Stojanović <78445216+Milos-Stojanovic@users.noreply.github.com>
2023-03-01 13:35:36 +0100

    Add model and repository for Impactors and DB simulation with JS files

---
+++ chain_impact_web/src/models/Impactor.tsx
@@ -0,0 +1,13 @@
+export type Impactor = {
+    wallet: string,
+    name: string,
+    description: string | null,
+    website: string | null,
+    facebook: string | null,
+    discord: string | null,
+    twitter: string | null,
+    instagram: string | null,
+    imageurl: string | null,
+    role: number,
+    type: number,
+}
---
+++ chain_impact_web/src/repositories/projectDb.js
@@ -0,0 +1,138 @@
+export const projectData = [
+    {
+        "id": 1,
+        "charity": {
+            "name": "United Nations",
+            "wallet": null,
+            "website": "un.org",
+            "facebook": null,
+            "discord": null,
+            "twitter": null,
+            "imageurl": null,
+            "description": "Help",
+            "id": 1
+        },
+        "name": "Turkish help",
+        "description": "help turky to survive",
+        "milestones": null,
+        "finantialgoal": 100000,
+        "totaldonated": 0,
+        "website": null,
+        "facebook": null,
+        "discord": null,
+        "twitter": null,
+        "instagram": null,
+        "imageurl": null,
+        "impactor": null,
+        "primarycausetype": {
+            "name": "health",
+            "id": 3
+        },
+        "secondarycausetype": {
+            "name": "environment",
+            "id": 1
+        }
+    },
+    {
+        "id": 2,
+        "charity": {
+            "name": "United Nations",
+            "wallet": null,
+            "website": "un.org",
+            "facebook": null,
+            "discord": null,
+            "twitter": null,
+            "imageurl": null,
+            "description": "Help",
+            "id": 1
+        },
+        "name": "Siria help",
+        "description": "help siria to survive",
+        "milestones": null,
+        "finantialgoal": 88000,
+        "totaldonated": 0,
+        "website": null,
+        "facebook": null,
+        "discord": null,
+        "twitter": null,
+        "instagram": null,
+        "imageurl": null,
+        "impactor": null,
+        "primarycausetype": {
+            "name": "health",
+            "id": 3
+        },
+        "secondarycausetype": {
+            "name": "environment",
+            "id": 1
+        }
+    },
+    {
+        "id": 3,
+        "charity": {
+            "name": "United Nations",
+            "wallet": null,
+            "website": "un.org",
+            "facebook": null,
+            "discord": null,
+            "twitter": null,
+            "imageurl": null,
+            "description": "Help",
+            "id": 1
+        },
+        "name": "Earthquake general help",
+        "description": null,
+        "milestones": null,
+        "finantialgoal": 15000,
+        "totaldonated": 0,
+        "website": null,
+        "facebook": null,
+        "discord": null,
+        "twitter": null,
+        "instagram": null,
+        "imageurl": null,
+        "impactor": null,
+        "primarycausetype": {
+            "name": "health",
+            "id": 3
+        },
+        "secondarycausetype": {
+            "name": "environment",
+            "id": 1
+        }
+    },
+    {
+        "id": 4,
+        "charity": {
+            "name": "Unesco",
+            "wallet": "sghruioghseruoghseuoghs",
+            "website": "unesco.org",
+            "facebook": null,
+            "discord": null,
+            "twitter": null,
+            "imageurl": null,
+            "description": "Help me!",
+            "id": 2
+        },
+        "name": "Cultural Heritage",
+        "description": "Money for restaturation",
+        "milestones": null,
+        "finantialgoal": 8655,
+        "totaldonated": 0,
+        "website": null,
+        "facebook": null,
+        "discord": null,
+        "twitter": null,
+        "instagram": null,
+        "imageurl": null,
+        "impactor": null,
+        "primarycausetype": {
+            "name": "social",
+            "id": 2
+        },
+        "secondarycausetype": {
+            "name": "education",
+            "id": 5
+        }
+    }
+]

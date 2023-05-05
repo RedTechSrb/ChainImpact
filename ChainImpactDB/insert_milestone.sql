0a0581d8fd0b8ddc33c973dbc71c7967591ded49
mdodovic <matija.dodovic@gmail.com>
2023-05-05 11:23:10 +0200

    insert order

--- ChainImpactDB/insert_milestone.sql
+++ ChainImpactDB/insert_milestone.sql
@@ -1,38 +1,38 @@
 -- Turkey Earthquake Relief
 
 INSERT INTO public.milestone(
-	name, description, 
+	name, order, description, 
 	complete, 
 	projectid)
-	VALUES ('Hire professors', 'Hire professors to teach your students.' 
+	VALUES ('Hire professors', 1, 'Hire professors to teach your students.' 
 			, 1679315696
 			, (select id from project where name like 'Turkey Earthquake Relief')
 			);
 
 
 INSERT INTO public.milestone(
-	name, description, 
+	name, order, description,  
 	complete, 
 	projectid)
-	VALUES ('Organize classes', 'Organize classes for your students, transport professors to a location' 
+	VALUES ('Organize classes', 2, 'Organize classes for your students, transport professors to a location' 
 			, 1680266096
 			, (select id from project where name like 'Turkey Earthquake Relief')
 			);
 
 INSERT INTO public.milestone(
-	name, description, 
+	name, order, description,  
 	complete, 
 	projectid)
-	VALUES ('Find the students in desired country', 'Find the students in desired country, and motivate and educate them to come to lectures.'
+	VALUES ('Find the students in desired country', 3, 'Find the students in desired country, and motivate and educate them to come to lectures.'
 			, 1681302896
 			, (select id from project where name like 'Turkey Earthquake Relief')
 			);
 
 INSERT INTO public.milestone(
-	name, description, 
+	name, order, description,  
 	complete, 
 	projectid)
-	VALUES ('Hold classes', 'Have more than 10000 students attend your classes.'
+	VALUES ('Hold classes', 4, 'Have more than 10000 students attend your classes.'
 			, null
 			, (select id from project where name like 'Turkey Earthquake Relief')
 			);
@@ -40,38 +40,38 @@ INSERT INTO public.milestone(
 -- Cancer Treatment for Children - St. Jude Children''s Research Hospital
 
 INSERT INTO public.milestone(
-	name, description, 
+	name, order, description,  
 	complete, 
 	projectid)
-	VALUES ('Hire professors', 'Hire professors to teach your students.' 
+	VALUES ('Hire professors', 1, 'Hire professors to teach your students.' 
 			, 1679315696
 			, (select id from project where name like 'Cancer Treatment for Children - St. Jude Children''s Research Hospital')
 			);
 
 
 INSERT INTO public.milestone(
-	name, description, 
+	name, order, description,  
 	complete, 
 	projectid)
-	VALUES ('Organize classes', 'Organize classes for your students, transport professors to a location' 
+	VALUES ('Organize classes', 2, 'Organize classes for your students, transport professors to a location' 
 			, 1680266096
 			, (select id from project where name like 'Cancer Treatment for Children - St. Jude Children''s Research Hospital')
 			);
 
 INSERT INTO public.milestone(
-	name, description, 
+	name, order, description,  
 	complete, 
 	projectid)
-	VALUES ('Find the students in desired country', 'Find the students in desired country, and motivate and educate them to come to lectures.'
+	VALUES ('Find the students in desired country', 3, 'Find the students in desired country, and motivate and educate them to come to lectures.'
 			, 1681302896
 			, (select id from project where name like 'Cancer Treatment for Children - St. Jude Children''s Research Hospital')
 			);
 
 INSERT INTO public.milestone(
-	name, description, 
+	name, order, description,  
 	complete, 
 	projectid)
-	VALUES ('Hold classes', 'Have more than 10000 students attend your classes.'
+	VALUES ('Hold classes', 4, 'Have more than 10000 students attend your classes.'
 			, null
 			, (select id from project where name like 'Cancer Treatment for Children - St. Jude Children''s Research Hospital')
 			);
@@ -79,38 +79,38 @@ INSERT INTO public.milestone(
 -- Innovative Sustainable Environment Education
 
 INSERT INTO public.milestone(
-	name, description, 
+	name, order, description,  
 	complete, 
 	projectid)
-	VALUES ('Hire professors', 'Hire professors to teach your students.' 
+	VALUES ('Hire professors', 1, 'Hire professors to teach your students.' 
 			, 1679315696
 			, (select id from project where name like 'Innovative Sustainable Environment Education')
 			);
 
 
 INSERT INTO public.milestone(
-	name, description, 
+	name, order, description,  
 	complete, 
 	projectid)
-	VALUES ('Organize classes', 'Organize classes for your students, transport professors to a location' 
+	VALUES ('Organize classes', 2, 'Organize classes for your students, transport professors to a location' 
 			, 1680266096
 			, (select id from project where name like 'Innovative Sustainable Environment Education')
 			);
 
 INSERT INTO public.milestone(
-	name, description, 
+	name, order, description,  
 	complete, 
 	projectid)
-	VALUES ('Find the students in desired country', 'Find the students in desired country, and motivate and educate them to come to lectures.'
+	VALUES ('Find the students in desired country', 3, 'Find the students in desired country, and motivate and educate them to come to lectures.'
 			, 1681302896
 			, (select id from project where name like 'Innovative Sustainable Environment Education')
 			);
 
 INSERT INTO public.milestone(
-	name, description, 
+	name, order, description,  
 	complete, 
 	projectid)
-	VALUES ('Hold classes', 'Have more than 10000 students attend your classes.'
+	VALUES ('Hold classes', 4, 'Have more than 10000 students attend your classes.'
 			, null
 			, (select id from project where name like 'Innovative Sustainable Environment Education')
 			);
@@ -119,38 +119,38 @@ INSERT INTO public.milestone(
 -- Education for All (EFA)
 
 INSERT INTO public.milestone(
-	name, description, 
+	name, order, description,  
 	complete, 
 	projectid)
-	VALUES ('Hire professors', 'Hire professors to teach your students.' 
+	VALUES ('Hire professors', 1, 'Hire professors to teach your students.' 
 			, 1679315696
 			, (select id from project where name like 'Education for All (EFA)')
 			);
 
 
 INSERT INTO public.milestone(
-	name, description, 
+	name, order, description,  
 	complete, 
 	projectid)
-	VALUES ('Organize classes', 'Organize classes for your students, transport professors to a location' 
+	VALUES ('Organize classes', 2, 'Organize classes for your students, transport professors to a location' 
 			, 1680266096
 			, (select id from project where name like 'Education for All (EFA)')
 			);
 
 INSERT INTO public.milestone(
-	name, description, 
+	name, order, description,  
 	complete, 
 	projectid)
-	VALUES ('Find the students in desired country', 'Find the students in desired country, and motivate and educate them to come to lectures.'
+	VALUES ('Find the students in desired country', 3, 'Find the students in desired country, and motivate and educate them to come to lectures.'
 			, 1681302896
 			, (select id from project where name like 'Education for All (EFA)')
 			);
 
 INSERT INTO public.milestone(
-	name, description, 
+	name, order, description,  
 	complete, 
 	projectid)
-	VALUES ('Hold classes', 'Have more than 10000 students attend your classes.'
+	VALUES ('Hold classes', 4, 'Have more than 10000 students attend your classes.'
 			, null
 			, (select id from project where name like 'Education for All (EFA)')
 			);

d4d60f753163b6fa63004267311c2cf974f37f92
DjordjeD <serbiasistem@gmail.com>
2023-05-25 14:53:27 +0200

    FixUSerProfileModal

--- chain_impact_web/src/components/Header.tsx
+++ chain_impact_web/src/components/Header.tsx
@@ -1,13 +1,17 @@
 import {
   Anchor,
+  Avatar,
   Box,
   Burger,
   Button,
   Center,
+  Checkbox,
   Collapse,
   createStyles,
   Divider,
   Drawer,
+  Flex,
+  Grid,
   Group,
   Header,
   HoverCard,
@@ -15,9 +19,12 @@ import {
   Modal,
   ScrollArea,
   Text,
+  Textarea,
+  TextInput,
   ThemeIcon,
   UnstyledButton,
 } from "@mantine/core";
+import { Dropzone, FileWithPath, IMAGE_MIME_TYPE } from "@mantine/dropzone";
 import { useDisclosure } from "@mantine/hooks";
 import { PublicKey, Transaction } from "@solana/web3.js";
 import {
@@ -28,7 +35,10 @@ import {
   IconCoin,
   IconFingerprint,
   IconNotification,
+  IconPhoto,
+  IconUpload,
   IconWallet,
+  IconX,
 } from "@tabler/icons";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
@@ -38,7 +48,7 @@ import {
   createNewImpactor,
   getSpecificImpactor,
 } from "../repositories/ImpactorRepository";
-import UserProfileModal from "./homeComponents/UserProfleModal";
+import UserProfileModal from "./homeComponents/UserProfileModal";
 import LightDarkMode from "./LightDarkMode";
 
 const useStyles: any = createStyles((theme) => ({
@@ -391,6 +401,43 @@ export default function HeaderResponsive({
     </UnstyledButton>
   ));
 
+  // USER MODAL
+
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [description, setDescription] = useState("");
+  const [isCompany, setIsCompany] = useState(false);
+  const [twitter, setTwitter] = useState("");
+  const [profileImage, setProfileImage] = useState<FileWithPath[]>([]);
+
+  const handleSubmit = () => {
+    // Perform form submission and user profile update here
+    // You can access the form values from the state variables (name, password, etc.)
+    // Handle any additional logic, validation, and API calls as needed
+    // Close the modal after the form is submitted successfully
+  };
+
+  const previews = profileImage.map((file, index) => {
+    const imageUrl = URL.createObjectURL(file);
+    return (
+      <Avatar
+        key={index}
+        src={imageUrl}
+        radius="xl"
+        imageProps={{ onLoad: () => URL.revokeObjectURL(imageUrl) }}
+        size="xl"
+      />
+    );
+  });
+
+  const placeholderAvatar = <Avatar radius="xl" size="xl" />;
+
+  const profileImagePreview =
+    profileImage.length > 0 ? previews : placeholderAvatar;
+
+  //
+
   return (
     <Box>
       <Header height={60} px="md" className={classes.header} id="header">
@@ -499,10 +546,114 @@ export default function HeaderResponsive({
               </a>
 
               <Button onClick={open}>Sign In</Button>
-              <UserProfileModal
-                isOpen={signInopened}
+              <Modal
+                opened={signInopened}
                 onClose={close}
-              ></UserProfileModal>
+                title="Add more info about yourself"
+                size={"xl"}
+              >
+                <Grid gutter="sm">
+                  <Grid.Col span={6}>
+                    <Flex
+                      gap="md"
+                      justify="center"
+                      direction="column"
+                      wrap="wrap"
+                    >
+                      <div>
+                        Profile Image
+                        {profileImagePreview}
+                      </div>
+                      <TextInput
+                        label="Username"
+                        value={name}
+                        onChange={(event) => setName(event.target.value)}
+                        required
+                      />
+                      <TextInput
+                        type="password"
+                        label="Password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                        required
+                      />
+                      <TextInput
+                        type="password"
+                        label="Confirm Password"
+                        value={confirmPassword}
+                        onChange={(event) =>
+                          setConfirmPassword(event.target.value)
+                        }
+                        required
+                      />
+                      <Textarea
+                        label="Description"
+                        value={description}
+                        onChange={(event) => setDescription(event.target.value)}
+                      />
+                      <TextInput
+                        label="Twitter"
+                        value={twitter}
+                        onChange={(event) => setTwitter(event.target.value)}
+                      />
+                      <Checkbox
+                        label="Company Account"
+                        checked={isCompany}
+                        onChange={() => setIsCompany(!isCompany)}
+                        description="Only companies can be an Angel Impactors. This way your profile will need to be verified by our team."
+                      />
+                      <Flex
+                        gap="sm"
+                        justify="center"
+                        align="center"
+                        direction="row"
+                        wrap="wrap"
+                      >
+                        <Button color="blue" onClick={handleSubmit}>
+                          Submit
+                        </Button>
+                        <Button variant="outline" onClick={close}>
+                          Cancel
+                        </Button>
+                      </Flex>
+                    </Flex>
+                  </Grid.Col>
+                  <Grid.Col span={6}>
+                    <Dropzone
+                      onDrop={setProfileImage}
+                      onReject={(files) => console.log("rejected files", files)}
+                      maxSize={3 * 1024 ** 2}
+                      accept={IMAGE_MIME_TYPE}
+                    >
+                      <Group
+                        position="center"
+                        spacing="xl"
+                        style={{ minHeight: 220, pointerEvents: "none" }}
+                      >
+                        <Dropzone.Accept>
+                          <IconUpload size={50} stroke={1.5} />
+                        </Dropzone.Accept>
+                        <Dropzone.Reject>
+                          <IconX size={50} stroke={1.5} />
+                        </Dropzone.Reject>
+                        <Dropzone.Idle>
+                          <IconPhoto size={50} stroke={1.5} />
+                        </Dropzone.Idle>
+
+                        <div>
+                          <Text size="xl" inline>
+                            Drag images here or click to select files
+                          </Text>
+                          <Text size="sm" color="dimmed" inline mt={7}>
+                            Attach as many files as you like, each file should
+                            not exceed 5mb
+                          </Text>
+                        </div>
+                      </Group>
+                    </Dropzone>
+                  </Grid.Col>
+                </Grid>
+              </Modal>
             </Group>
           </Group>
 

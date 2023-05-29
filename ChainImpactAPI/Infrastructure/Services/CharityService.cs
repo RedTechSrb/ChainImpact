97f359b7535ac477692ea615965c6544f7f3c5bc
mdodovic <matija.dodovic@gmail.com>
2023-05-29 18:07:03 +0200

    Charity service

--- ChainImpactAPI/Infrastructure/Services/CharityService.cs
+++ ChainImpactAPI/Infrastructure/Services/CharityService.cs
@@ -2,6 +2,8 @@
 using ChainImpactAPI.Application.ServiceInterfaces;
 using ChainImpactAPI.Dtos;
 using ChainImpactAPI.Dtos.SearchDtos;
+using ChainImpactAPI.Infrastructure.Repositories;
+using ChainImpactAPI.Models;
 
 namespace ChainImpactAPI.Infrastructure.Services
 {
@@ -18,9 +20,9 @@ namespace ChainImpactAPI.Infrastructure.Services
             this.charityRepository = charityRepository;
         }
 
-        public List<CharityDto> SearchCharities(GenericDto<CharitySearchDto>? charitySearchDto)
+        public List<CharityDto> SearchCharities(GenericDto<CharityDto>? charityDto)
         {
-            var charities = charityRepository.SearchAsync(charitySearchDto).Result;
+            var charities = charityRepository.SearchAsync(charityDto).Result;
 
             var charityDtoList = new List<CharityDto>();
             foreach (var charity in charities)
@@ -42,5 +44,40 @@ namespace ChainImpactAPI.Infrastructure.Services
 
 
         }
+
+        public Charity SaveCharity(CharityDto charityDto)
+        {
+            var charity = charityRepository.SearchAsync(new GenericDto<CharityDto>(new CharityDto{ name = charityDto.name })).Result.FirstOrDefault();
+            
+            if(charity == null)
+            {
+                charity = new Charity 
+                { 
+                    description= charityDto.description,
+                    discord= charityDto.discord,
+                    imageurl= charityDto.imageurl,
+                    twitter= charityDto.twitter,
+                    facebook= charityDto.facebook,
+                    website= charityDto.twitter,
+                    wallet = charityDto.wallet,
+                    name = charityDto.name
+                };
+            }
+            else
+            {
+                // Update charity, only following fields can be updated
+                // charity.name = charityDto.name == null ? charity.name : charityDto.name;
+                charity.wallet = charityDto.wallet == null ? charity.wallet : charityDto.wallet;
+                charity.website = charityDto.website == null ? charity.website : charityDto.website;
+                charity.facebook = charityDto.facebook == null ? charity.facebook : charityDto.facebook;
+                charity.discord = charityDto.discord == null ? charity.discord : charityDto.discord;
+                charity.twitter = charityDto.twitter == null ? charity.twitter : charityDto.twitter;
+                charity.imageurl = charityDto.imageurl == null ? charity.imageurl : charityDto.imageurl;
+                charity.description = charityDto.description == null ? charity.description : charityDto.description;
+            }
+
+            return charityRepository.Save(charity);
+        }
+
     }
 }

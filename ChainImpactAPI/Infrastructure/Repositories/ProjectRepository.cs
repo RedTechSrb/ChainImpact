63e3fa8e2f89910933cc89d63bf3351372672f39
mdodovic <matija.dodovic@gmail.com>
2023-05-31 17:16:53 +0200

    Project search add

--- ChainImpactAPI/Infrastructure/Repositories/ProjectRepository.cs
+++ ChainImpactAPI/Infrastructure/Repositories/ProjectRepository.cs
@@ -43,7 +43,76 @@ namespace ChainImpactAPI.Infrastructure.Repositories
             {
                 projects = projects.Where(p => p.id == projectSearch.id).ToList();
             }
-            // TODO: Add other searchable parameters
+            if (projectSearch.name != null)
+            {
+                projects = projects.Where(p => p.name == projectSearch.name).ToList();
+            }
+            if (projectSearch.wallet != null)
+            {
+                projects = projects.Where(p => p.wallet == projectSearch.wallet).ToList();
+            }
+            if (projectSearch.description != null)
+            {
+                projects = projects.Where(p => p.description == projectSearch.description).ToList();
+            }
+            if (projectSearch.financialgoal != null)
+            {
+                projects = projects.Where(p => p.financialgoal == projectSearch.financialgoal).ToList();
+            }
+            if (projectSearch.totaldonated != null)
+            {
+                projects = projects.Where(p => p.totaldonated == projectSearch.totaldonated).ToList();
+            }
+            if (projectSearch.totalbackers != null)
+            {
+                projects = projects.Where(p => p.totalbackers == projectSearch.totalbackers).ToList();
+            }
+            if (projectSearch.website != null)
+            {
+                projects = projects.Where(p => p.website == projectSearch.website).ToList();
+            }
+            if (projectSearch.facebook != null)
+            {
+                projects = projects.Where(p => p.facebook == projectSearch.facebook).ToList();
+            }
+            if (projectSearch.discord != null)
+            {
+                projects = projects.Where(p => p.discord == projectSearch.discord).ToList();
+            }
+            if (projectSearch.twitter != null)
+            {
+                projects = projects.Where(p => p.twitter == projectSearch.twitter).ToList();
+            }
+            if (projectSearch.instagram != null)
+            {
+                projects = projects.Where(p => p.instagram == projectSearch.instagram).ToList();
+            }
+            if (projectSearch.imageurl != null)
+            {
+                projects = projects.Where(p => p.imageurl == projectSearch.imageurl).ToList();
+            }
+            if (projectSearch.confirmed != null)
+            {
+                projects = projects.Where(p => p.confirmed == projectSearch.confirmed).ToList();
+            }
+
+            if (projectSearch.charity != null)
+            {
+                projects = projects.Where(p => p.charity.id == projectSearch.charity.id).ToList();
+            }
+            if (projectSearch.primarycausetype != null)
+            {
+                projects = projects.Where(p => p.primarycausetype.id == projectSearch.primarycausetype.id).ToList();
+            }
+            if (projectSearch.secondarycausetype != null)
+            {
+                projects = projects.Where(p => p.secondarycausetype.id == projectSearch.secondarycausetype.id).ToList();
+            }
+            if (projectSearch.angelimpactor != null)
+            {
+                projects = projects.Where(p => (p.impactor != null && p.impactor.id == projectSearch.angelimpactor.id)).ToList();
+            }
+
 
             if (skip != null && take != null)
             {
